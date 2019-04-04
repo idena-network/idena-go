a6158439eec3dbc28b9b6bc19a07c15700c77323
ridenaio <r@idena.io>
2019-04-04 15:12:17 +0500

    add flips lottery

---
+++ blockchain/lottery.go
@@ -0,0 +1,38 @@
+package blockchain
+
+import (
+	"encoding/binary"
+	"math/rand"
+)
+
+func SortFlips(candidatesLen int, flipsPerAddr int, seed []byte) (flipsPerCandidate [][]int, candidatesPerFlips [][]int) {
+	groupsCount := candidatesLen / flipsPerAddr
+
+	flipsPerCandidate = make([][]int, candidatesLen)
+	candidatesPerFlips = make([][]int, candidatesLen)
+
+	x := binary.LittleEndian.Uint64(seed)
+
+	for i := 0; i < flipsPerAddr; i++ {
+
+		s := x + x%21*uint64(i+1)
+
+		perm := rand.New(rand.NewSource(int64(s))).Perm(candidatesLen)
+
+		for g := 0; g < groupsCount; g++ {
+			for k := 0; k < flipsPerAddr; k++ {
+				flipIdx := g + i*groupsCount
+				flipsPerCandidate[perm[g*flipsPerAddr+k]] = append(flipsPerCandidate[perm[g*flipsPerAddr+k]], flipIdx)
+				candidatesPerFlips[flipIdx] = append(candidatesPerFlips[flipIdx], perm[g*flipsPerAddr+k])
+			}
+		}
+		for d := 0; d < candidatesLen%flipsPerAddr; d++ {
+			candidateIdx := groupsCount*flipsPerAddr + d
+			flipIdx := d%groupsCount + i*groupsCount
+			flipsPerCandidate[perm[candidateIdx]] = append(flipsPerCandidate[perm[candidateIdx]], flipIdx)
+			candidatesPerFlips[flipIdx] = append(candidatesPerFlips[flipIdx], perm[candidateIdx])
+		}
+	}
+
+	return flipsPerCandidate, candidatesPerFlips
+}
---
+++ blockchain/lottery_test.go
@@ -0,0 +1,49 @@
+package blockchain
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSortFlips(t *testing.T) {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, 500)
+
+	flipsPerCandidate, candidatesPerFlips := SortFlips(7, 3, b)
+
+	flipsPerCandidateResult := [][]int{{1, 3, 4}, {1, 2, 4}, {1, 3, 5}, {0, 2, 4}, {0, 2, 5}, {0, 3, 4}, {0, 2, 5}}
+	candidatesPerFlipsResult := [][]int{{5, 3, 6, 4}, {0, 2, 1}, {3, 1, 6, 4}, {0, 2, 5}, {0, 1, 5, 3}, {6, 4, 2}, []int(nil)}
+
+	require.Equal(t, flipsPerCandidateResult, flipsPerCandidate)
+	require.Equal(t, candidatesPerFlipsResult, candidatesPerFlips)
+}
+
+func TestSortFlips_Big(t *testing.T) {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, 500)
+
+	const flipPerAddress = 15
+
+	flipsPerCandidate, _ := SortFlips(10000, flipPerAddress, b)
+
+	for i := 0; i < len(flipsPerCandidate); i++ {
+		require.Equal(t, flipPerAddress, len(flipsPerCandidate[i]))
+	}
+}
+
+func TestSortFlips_Small(t *testing.T) {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, 123123123)
+
+	flipsPerCandidate, candidatesPerFlips := SortFlips(5, 2, b)
+
+	fmt.Println(flipsPerCandidate)
+	fmt.Println(candidatesPerFlips)
+	flipsPerCandidateResult := [][]int{{0, 3}, {1, 2}, {0, 2}, {1, 3}, {0, 2}}
+	candidatesPerFlipsResult := [][]int{{4, 2, 0}, {3, 1}, {1, 4, 2}, {3, 0}, []int(nil)}
+
+	require.Equal(t, flipsPerCandidateResult, flipsPerCandidate)
+	require.Equal(t, candidatesPerFlipsResult, candidatesPerFlips)
+}
