ff300a52413b11b0dbece05ba038eff19662d502
mbidenaio <50830972+mbidenaio@users.noreply.github.com>
2019-06-04 18:35:50 +0500

    Add sorting txs in block by type priority (#9)

--- core/mempool/txpool.go
+++ core/mempool/txpool.go
@@ -20,6 +20,13 @@ const (
 
 var (
 	DuplicateTxError = errors.New("tx with same hash already exists")
+
+	priorityTypes = map[types.TxType]bool{
+		types.SubmitAnswersHashTx:  true,
+		types.SubmitShortAnswersTx: true,
+		types.SubmitLongAnswersTx:  true,
+		types.EvidenceTx:           true,
+	}
 )
 
 type TxPool struct {
@@ -121,54 +128,10 @@ func (txpool *TxPool) GetTx(hash common.Hash) *types.Transaction {
 }
 
 func (txpool *TxPool) BuildBlockTransactions() []*types.Transaction {
-	var list []*types.Transaction
-	var result []*types.Transaction
-
-	txpool.mutex.Lock()
-	for _, tx := range txpool.pending {
-		list = append(list, tx)
-	}
-	txpool.mutex.Unlock()
-
-	sort.SliceStable(list, func(i, j int) bool {
-		if list[i].Epoch < list[j].Epoch {
-			return true
-		}
-		if list[i].Epoch > list[j].Epoch {
-			return false
-		}
-		return list[i].AccountNonce < list[j].AccountNonce
-	})
-
-	currentNonces := make(map[common.Address]uint32)
-	globalEpoch := txpool.appState.State.Epoch()
-	size := 0
-	for _, tx := range list {
-		if size > BlockBodySize {
-			break
-		}
-		sender, _ := types.Sender(tx)
-		if tx.Epoch < globalEpoch {
-			continue
-		}
-		// do not process transactions from future epoch
-		if tx.Epoch > globalEpoch {
-			break
-		}
-		if _, ok := currentNonces[sender]; !ok {
-			currentNonces[sender] = txpool.appState.State.GetNonce(sender)
-			if txpool.appState.State.GetEpoch(sender) < globalEpoch {
-				currentNonces[sender] = 0
-			}
-		}
-		if currentNonces[sender]+1 == tx.AccountNonce {
-			result = append(result, tx)
-			size += tx.Size()
-			currentNonces[sender] = tx.AccountNonce
-		}
-	}
-
-	return result
+	ctx := txpool.createBuildingContext()
+	ctx.addPriorityTxsToBlock()
+	ctx.addTxsToBlock()
+	return ctx.blockTxs
 }
 
 func (txpool *TxPool) Remove(transaction *types.Transaction) {
@@ -221,4 +184,46 @@ func (txpool *TxPool) checkAddrTxLimit(sender common.Address) error {
 		return errors.New("address tx queue max size reached")
 	}
 	return nil
+}
+
+func (txpool *TxPool) createBuildingContext() *buildingContext {
+	curNoncesPerSender := make(map[common.Address]uint32)
+	var txs []*types.Transaction
+	txpool.mutex.Lock()
+	globalEpoch := txpool.appState.State.Epoch()
+	withPriorityTx := false
+	for _, tx := range txpool.pending {
+		if tx.Epoch != globalEpoch {
+			continue
+		}
+		txs = append(txs, tx)
+		withPriorityTx = withPriorityTx || priorityTypes[tx.Type]
+	}
+	for sender, _ := range txpool.pendingPerAddr {
+		if txpool.appState.State.GetEpoch(sender) < globalEpoch {
+			curNoncesPerSender[sender] = 0
+		} else {
+			curNoncesPerSender[sender] = txpool.appState.State.GetNonce(sender)
+		}
+	}
+	txpool.mutex.Unlock()
+
+	sort.SliceStable(txs, func(i, j int) bool {
+		return txs[i].AccountNonce < txs[j].AccountNonce
+	})
+
+	var priorityTxs []*types.Transaction
+	var sortedTxsPerSender map[common.Address][]*types.Transaction
+	if withPriorityTx {
+		sortedTxsPerSender = make(map[common.Address][]*types.Transaction)
+		for _, tx := range txs {
+			if priorityTypes[tx.Type] {
+				priorityTxs = append(priorityTxs, tx)
+			}
+			sender, _ := types.Sender(tx)
+			sortedTxsPerSender[sender] = append(sortedTxsPerSender[sender], tx)
+		}
+	}
+
+	return newBuildingContext(txs, priorityTxs, sortedTxsPerSender, curNoncesPerSender)
 }
