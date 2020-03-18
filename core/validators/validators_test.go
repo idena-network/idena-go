2fa4b8a4504d9f7a18e7cecf0ab15f366af12429
sidenaio <50700001+sidenaio@users.noreply.github.com>
2020-03-18 21:18:57 +0500

    Fix concurrency error in block validation (#316)

--- core/validators/validators_test.go
+++ core/validators/validators_test.go
@@ -47,3 +47,45 @@ func TestValidatorsCache_Contains(t *testing.T) {
 	require.Equal(countOnline, vCache.OnlineSize())
 	require.Equal(countAll, vCache.NetworkSize())
 }
+
+
+func TestValidatorsCache_Clone(t *testing.T) {
+	require := require.New(t)
+	database := db.NewMemDB()
+	identityStateDB := state.NewLazyIdentityState(database)
+
+	m := make(map[common.Address]bool)
+
+	countOnline, countAll := 0, 100
+
+	for j := 0; j < countAll; j++ {
+		key, _ := crypto.GenerateKey()
+		addr := crypto.PubkeyToAddress(key.PublicKey)
+
+		obj := identityStateDB.GetOrNewIdentityObject(addr)
+		obj.SetState(true)
+
+		isOnline := rand.Int31()%2 == 0
+		m[addr] = isOnline
+
+		if isOnline {
+			obj.SetOnline(true)
+			countOnline++
+		}
+	}
+	identityStateDB.Commit(false)
+
+	vCache := NewValidatorsCache(identityStateDB, common.Address{0x1})
+	vCache.Load()
+
+	clone := vCache.Clone()
+
+
+	require.Equal(vCache.height, clone.height)
+	require.Equal(vCache.god, clone.god)
+	require.Equal(vCache.validOnlineNodes, clone.validOnlineNodes)
+	require.Equal(vCache.onlineNodesSet, clone.onlineNodesSet)
+	require.Equal(vCache.nodesSet, clone.nodesSet)
+	require.False(vCache.onlineNodesSet == clone.onlineNodesSet)
+	require.False(vCache.nodesSet == clone.nodesSet)
+}
