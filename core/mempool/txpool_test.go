03e207d3bc174b9c2e7532e8d200af6f3f466dfb
sidenaio <st@idena.io>
2020-04-09 19:10:32 +0500

    add new test

--- core/mempool/txpool_test.go
+++ core/mempool/txpool_test.go
@@ -1,6 +1,7 @@
 package mempool
 
 import (
+	"crypto/ecdsa"
 	"github.com/idena-network/idena-go/blockchain/types"
 	"github.com/idena-network/idena-go/common"
 	"github.com/idena-network/idena-go/common/eventbus"
@@ -65,14 +66,108 @@ func TestTxPool_addDeferredTx(t *testing.T) {
 	r.Len(pool.executableTxs, 1)
 }
 
+func TestTxPool_ResetTo(t *testing.T) {
+	pool := getPool()
+
+	keys := make([]*ecdsa.PrivateKey, 0)
+
+	for i := 0; i < 1200; i++ {
+		key, _ := crypto.GenerateKey()
+		address := crypto.PubkeyToAddress(key.PublicKey)
+		keys = append(keys, key)
+		pool.appState.State.SetBalance(address, big.NewInt(0).Mul(big.NewInt(10000), common.DnaBase))
+	}
+
+	pool.appState.Commit(nil)
+	pool.appState.Initialize(1)
+	pool.head = &types.Header{
+		EmptyBlockHeader: &types.EmptyBlockHeader{
+			Height: 1,
+		},
+	}
+	getTx := func(key *ecdsa.PrivateKey) *types.Transaction {
+		address := crypto.PubkeyToAddress(key.PublicKey)
+
+		nonce := pool.appState.NonceCache.GetNonce(address, 0)
+
+		tx := &types.Transaction{
+			AccountNonce: nonce + 1,
+			To:           &address,
+			Epoch:        0,
+			Type:         types.SendTx,
+			Amount:       new(big.Int).Mul(common.DnaBase, big.NewInt(1)),
+		}
+
+		tx, _ = types.SignTx(tx, key)
+		return tx
+	}
+
+	for i := 0; i < 255; i++ {
+		for j := 0; j < 35; j++ {
+			require.NoError(t, pool.Add(getTx(keys[i])))
+		}
+	}
+	for i := 255; i < 1024; i++ {
+		for j := 0; j < 32; j++ {
+			require.NoError(t, pool.Add(getTx(keys[i])))
+		}
+	}
+	for i := 1023; i < 1200; i++ {
+		require.NoError(t, pool.Add(getTx(keys[i])))
+	}
+	allTxsCount := len(pool.all.txs)
+
+	require.Len(t, pool.executableTxs, 1200)
+	require.Len(t, pool.pendingTxs, 256)
+
+	full := true
+	for _, e := range pool.executableTxs {
+		full = full && (len(e.txs) == 32 || len(e.txs) == 1)
+	}
+	require.True(t, full)
+
+	assertTxs := func() {
+		cnt := 0
+		for _, txs := range pool.executableTxs {
+			cnt += len(txs.txs)
+		}
+		for _, txs := range pool.pendingTxs {
+			cnt += len(txs.txs)
+		}
+		require.Equal(t, cnt, len(pool.all.txs))
+	}
+	assertTxs()
+	for height := 2; height <= 5; height++ {
+
+		builtTxs := pool.BuildBlockTransactions()
+		require.True(t, len(builtTxs) > 0)
+		for _, tx := range builtTxs {
+			sender, _ := types.Sender(tx)
+			pool.appState.State.SetNonce(sender, tx.AccountNonce)
+		}
+		pool.appState.Commit(nil)
+
+		pool.ResetTo(&types.Block{
+			Header: &types.Header{ProposedHeader: &types.ProposedHeader{
+				Height: uint64(height),
+			}},
+			Body: &types.Body{
+				Transactions: builtTxs,
+			},
+		})
+		require.Equal(t, allTxsCount-len(builtTxs), len(pool.all.txs))
+		allTxsCount = len(pool.all.txs)
+		assertTxs()
+	}
+
+	require.Len(t, pool.all.txs, 0)
+	require.Len(t, pool.executableTxs, 0)
+	require.Len(t, pool.pendingTxs, 0)
+}
+
 func getPool() *TxPool {
 	bus := eventbus.New()
 	appState := appstate.NewAppState(db.NewMemDB(), bus)
-
-	key, _ := crypto.GenerateKey()
-	secStore := secstore.NewSecStore()
-	secStore.AddKey(crypto.FromECDSA(key))
-
 	return NewTxPool(appState, bus, config.GetDefaultMempoolConfig(), big.NewInt(0))
 }
 
