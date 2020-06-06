27d3252124229ee00aa55484c2e965d580e30970
sidenaio <50700001+sidenaio@users.noreply.github.com>
2020-06-06 15:17:46 +0500

    Improve proposals validation (#461)

--- pengings/proposals_test.go
+++ pengings/proposals_test.go
@@ -2,44 +2,11 @@ package pengings
 
 import (
 	"github.com/idena-network/idena-go/blockchain/types"
-	"github.com/idena-network/idena-go/common"
-	"github.com/idena-network/idena-go/crypto"
 	"github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/require"
-	"sync"
 	"testing"
 	"time"
 )
-
-func TestProposals_GetProposerPubKey(t *testing.T) {
-
-	proposals := &Proposals{
-		proofsByRound: &sync.Map{},
-	}
-	byRound := &sync.Map{}
-	proposals.proofsByRound.Store(uint64(1), byRound)
-
-	hashes := []common.Hash{{0x1}, {0x1, 0x2}, {0x3, 0x2}, {0x2, 0x2}, {0x2}}
-
-	var pubKeys [][]byte
-	for i := 0; i < 5; i++ {
-		key, _ := crypto.GenerateKey()
-
-		proofProposal := &types.ProofProposal{
-			Proof: []byte{byte(i)},
-			Round: 1,
-		}
-
-		hash := crypto.SignatureHash(proofProposal)
-		sig, _ := crypto.Sign(hash[:], key)
-		proofProposal.Signature = sig
-		byRound.Store(hashes[i], proofProposal)
-
-		pubKeys = append(pubKeys, crypto.FromECDSAPub(&key.PublicKey))
-	}
-
-	require.Equal(t, pubKeys[2], proposals.GetProposerPubKey(1))
-}
 
 func TestProposals_GetBlock(t *testing.T) {
 	proposals := &Proposals{
