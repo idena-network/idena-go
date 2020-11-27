338b4f6f9e6b5bf4675e9ab7f6fe04cd5d47ee8a
RealMahmoud <RealMahmoudm@gmail.com>
2020-11-27 12:33:49 +0200

    Update dna_api.go

--- api/dna_api.go
+++ api/dna_api.go
@@ -151,7 +151,7 @@ func (api *DnaApi) ActivateInvite(ctx context.Context, args ActivateInviteArgs) 
 	} else {
 		pubKey = api.baseApi.secStore.GetPubKey()
 	}
-	to, err := crypto.PubKeyBytesToAddress(pubKey)
+	to := crypto.PubKeyBytesToAddress(pubKey)
 	hash, err := api.baseApi.sendTx(ctx, from, &to, types.ActivationTx, decimal.Zero, decimal.Zero, decimal.Zero, args.Nonce, args.Epoch, pubKey, key)
 
 	if err != nil {
