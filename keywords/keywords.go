ec50981be4e7e9a85f5e920e420ff215dcef407f
sidenaio <st@idena.io>
2021-04-25 17:43:15 +0500

    Add flip keywords

---
+++ keywords/keywords.go
@@ -0,0 +1,29 @@
+package keywords
+
+import (
+	"encoding/json"
+	"github.com/idena-network/idena-go/log"
+	"github.com/pkg/errors"
+)
+
+type Keyword struct {
+	Name string
+	Desc string
+}
+
+var list []Keyword
+
+func init() {
+	list = make([]Keyword, 0, 3350)
+	data, _ := Asset("keywords.json")
+	if err := json.Unmarshal(data, &list); err != nil {
+		log.Warn("cannot parse keywords.json", "err", err)
+	}
+}
+
+func Get(index int) (Keyword, error) {
+	if index >= len(list) || index < 0 {
+		return Keyword{}, errors.New("index is out of range")
+	}
+	return list[index], nil
+}
