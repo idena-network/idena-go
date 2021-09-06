a9c899956a8366b87140f76f983f4fa84932c5d5
mbidenaio <mb@idena.io>
2021-09-06 23:18:13 +0500

    Add tests

--- core/ceremony/reporters_test.go
+++ core/ceremony/reporters_test.go
@@ -129,3 +129,41 @@ func Test_setValidationResult(t *testing.T) {
 	require.Equal(t, 0, len(r.reportersByAddr))
 	require.Equal(t, 0, len(r.reportersByFlip))
 }
+
+func Test_setValidationResult2(t *testing.T) {
+	r := newReportersToReward()
+	addr1 := common.Address{1}
+	addr2 := common.Address{2}
+	addr3 := common.Address{3}
+	addr4 := common.Address{4}
+
+	r.addReport(2, addr1)
+	r.addReport(3, addr1)
+	r.addReport(2, addr2)
+
+	flipsByAuthor := map[common.Address][]int{
+		addr3: {1, 2},
+		addr4: {3, 4},
+	}
+
+	conf := &config.ConsensusConf{
+		ReportsRewardPercent: 0.01,
+	}
+
+	r.setValidationResult(addr1, state.Newbie, false, flipsByAuthor, conf)
+	require.Equal(t, uint8(state.Newbie), r.reportersByAddr[addr1].NewIdentityState)
+
+	r.setValidationResult(addr2, state.Human, false, flipsByAuthor, conf)
+	require.Equal(t, uint8(state.Human), r.reportersByAddr[addr2].NewIdentityState)
+
+	r.setValidationResult(addr1, state.Killed, false, flipsByAuthor, conf)
+	require.Equal(t, 1, len(r.reportedFlipsByReporter))
+	require.Equal(t, 1, len(r.reportersByAddr))
+	require.NotContains(t, r.reportersByAddr, addr1)
+	require.NotContains(t, r.reportedFlipsByReporter, addr1)
+
+	r.setValidationResult(addr3, state.Suspended, true, flipsByAuthor, conf)
+	require.Equal(t, 1, len(r.reportedFlipsByReporter))
+	require.Equal(t, 1, len(r.reportersByAddr))
+	require.Equal(t, 1, len(r.reportersByFlip))
+}
