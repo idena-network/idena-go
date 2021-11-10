18e1cd5471209bf95783205be5cf8851b8f36ddd
sidenaio <50700001+sidenaio@users.noreply.github.com>
2021-11-10 15:22:10 +0500

    Increase fd limit (#854)

--- common/ulimit/ulimit_test.go
+++ common/ulimit/ulimit_test.go
@@ -8,7 +8,9 @@ import (
 
 func TestManageFdLimit(t *testing.T) {
 	t.Log("Testing file descriptor count")
-	if _, _, err := ManageFdLimit(); err != nil {
+	if changed, newLimit, err := ManageFdLimit(); err != nil {
 		t.Errorf("Cannot manage file descriptors - %v", err)
+	} else if changed {
+		t.Logf("Setup new fd limit - %v", newLimit)
 	}
 }
