1b5dcd5e3d118f3d041e8f1122ddc09b4e0db044
mbidenaio <50830972+mbidenaio@users.noreply.github.com>
2022-08-18 14:45:07 +0500

    Allow to finish voting when it has quorum and all votes are open (#999)

--- vm/embedded/embedded_test.go
+++ vm/embedded/embedded_test.go
@@ -188,7 +188,7 @@ func (c *contractTester) createContract(ctx env.CallContext, e env.Env) Contract
 	case TimeLockContract:
 		return NewTimeLock(ctx, e, nil)
 	case OracleVotingContract:
-		return NewOracleVotingContract5(ctx, e, nil)
+		return NewOracleVotingContract6(ctx, e, nil)
 	case OracleLockContract:
 		return NewOracleLock2(ctx, e, nil)
 	case RefundableOracleLockContract:
