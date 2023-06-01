1eae18da8be88157f35c773f6c89547046f49b33
mbidenaio <mb@idena.io>
2023-06-01 16:43:00 +0500

    Fix test

--- config/validation_test.go
+++ config/validation_test.go
@@ -10,12 +10,12 @@ func TestGetNextValidationTime(t *testing.T) {
 	conf := &ValidationConfig{}
 	validationTime := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
 
-	nextValidationTime := conf.GetNextValidationTime(validationTime, 449)
-	require.Equal(t, time.Date(2020, 1, 9, 0, 0, 0, 0, time.UTC),
+	nextValidationTime := conf.GetNextValidationTime(validationTime, 449, true)
+	require.Equal(t, time.Date(2020, 1, 14, 0, 0, 0, 0, time.UTC),
 		nextValidationTime)
 
 	conf.ValidationInterval = 2*time.Hour + time.Minute*3
-	nextValidationTime = conf.GetNextValidationTime(validationTime, 449)
+	nextValidationTime = conf.GetNextValidationTime(validationTime, 449, true)
 	require.Equal(t, time.Date(2020, 1, 1, 2, 3, 0, 0, time.UTC),
 		nextValidationTime)
 }
