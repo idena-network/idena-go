3293828dac951c3655f3e55de8ab23b46ed077a1
mbidenaio <50830972+mbidenaio@users.noreply.github.com>
2023-07-04 11:24:26 +0500

    Change the time of validation ceremony (#1134)

--- config/validation_test.go
+++ config/validation_test.go
@@ -14,6 +14,14 @@ func TestGetNextValidationTime(t *testing.T) {
 	require.Equal(t, time.Date(2020, 1, 16, 0, 0, 0, 0, time.UTC),
 		nextValidationTime)
 
+	nextValidationTime = conf.GetNextValidationTime(time.Date(2020, 1, 1, 13, 30, 0, 0, time.UTC), 449, true)
+	require.Equal(t, time.Date(2020, 1, 16, 15, 0, 0, 0, time.UTC),
+		nextValidationTime)
+
+	nextValidationTime = conf.GetNextValidationTime(time.Date(2020, 1, 1, 15, 0, 0, 0, time.UTC), 449, true)
+	require.Equal(t, time.Date(2020, 1, 16, 15, 0, 0, 0, time.UTC),
+		nextValidationTime)
+
 	conf.ValidationInterval = 2*time.Hour + time.Minute*3
 	nextValidationTime = conf.GetNextValidationTime(validationTime, 449, true)
 	require.Equal(t, time.Date(2020, 1, 1, 2, 3, 0, 0, time.UTC),
